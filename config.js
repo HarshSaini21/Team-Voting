52f75a828151a71e5a692bb2d9729c8921df2934
HarshSaini21 <harshdeepsingh8451@sjcsjal.org>
2021-12-24 18:36:17 +0530

    Project67

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
 
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAqF33Y0BIowsc8BPGBtu5Au7Vxt0sTpBM",
+  authDomain: "team-voting-2d0b1.firebaseapp.com",
+  projectId: "team-voting-2d0b1",
+  storageBucket: "team-voting-2d0b1.appspot.com",
+  messagingSenderId: "15475721635",
+  appId: "1:15475721635:web:8d23190d3d1ced949d8616"
 };
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export default firebase.database();
